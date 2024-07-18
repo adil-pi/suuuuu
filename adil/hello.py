97272363bfe1336b8bcc9c91d593495fc6ab42e7
adil-pi <bakinovadil@gmail.com>
2024-07-18 19:00:54 +0600

    Add files via upload

---
+++ adil/hello.py
@@ -0,0 +1,47 @@
+# value = int(input("Введите число: "))
+# result = 10 / value
+# print(result)
+
+# Возможные ошибки
+
+# try:   
+#     value = int(input("Введите число!"))
+#     result = 10 / value
+# except ValueError:
+#     print("Ошибка! Введено не число")
+# except ZeroDivisionError:
+#     print("Ошибка! На ноль делить нельзя!")
+# else:
+#     print(f"Рузультат: {result}")
+# finally:   
+#     print("Блок finally выполнен!")
+
+
+
+# age: int = 21
+# name: str = "Sten"
+# is_student: bool = True
+
+# print(age, name, is_student)
+
+# def add(a: int, b: int) -> int:
+#     return a + b
+
+# print(add(10, 20))
+
+
+
+with open('example.text', 'w') as file:
+    file.write("hello, world!\n")
+    file.write("this is super text!\n")
+    file.write("very much information!\n")
+    
+
+
+
+
+
+
+
+
+
